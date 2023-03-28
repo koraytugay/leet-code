535d5cb126546f87ea98a264d9c79da36138db52
Koray Tugay <ktugay@sonatype.com>
2023-03-27 21:48:07 -0400

    MoveZeros.java

--- src/biz/tugay/leetcode/array/MoveZeros.java
+++ src/biz/tugay/leetcode/array/MoveZeros.java
@@ -4,24 +4,19 @@ package biz.tugay.leetcode.array;
 public class MoveZeros {
 
   public void moveZeroes(int[] nums) {
-    int j = -1;
-    outer:
-    for (int i = 0; i < nums.length - 1; i++) {
-      if (nums[i] != 0) {
-        continue;
-      }
-      if (j == -1) {
-        j = i + 1;
-      }
-      for (; j < nums.length; j++) {
-        if (nums[j] != 0) {
-          nums[i] = nums[j];
-          nums[j] = 0;
-          break;
+    for (int slow = 0, fast = 0; slow < nums.length - 1; slow++) {
+      if (nums[slow] == 0) {
+        while (nums[fast] == 0) {
+          fast++;
+          if (fast == nums.length) {
+            return;
+          }
         }
-        if (j == nums.length - 1) {
-          break outer;
-        }
+        nums[slow] = nums[fast];
+        nums[fast] = 0;
+      }
+      if (fast == slow) {
+        fast++;
       }
     }
   }
